964fc7cb3b1122dc19b14de77f27b17a0d6cee4e
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-02-04 12:11:57 -0300

    refatora para ter midleware de autenticaçao e facilitar minha vida

---
+++ middlewares/authmiddleware.js
@@ -0,0 +1,23 @@
+const { verifyToken } = require('../services/authService');
+const { createErrorMessage } = require('../services/utils/validateCreateUserCredentials');
+
+const TOKEN_INVALID_MESSAGE = 'Expired or invalid token';
+const TOKEN_NOT_FOUND_MESSAGE = 'Token not found';
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    const answerOBJ = createErrorMessage(TOKEN_NOT_FOUND_MESSAGE, 401);
+    return res.status(answerOBJ.status).json(answerOBJ.answer);
+  }
+  const tokenVerified = verifyToken(token);
+  if (!tokenVerified) {
+    const answerOBJ = createErrorMessage(TOKEN_INVALID_MESSAGE, 401);
+    return res.status(answerOBJ.status).json(answerOBJ.answer);
+  }
+
+  next();
+};
+
+module.exports = authMiddleware;
